f74309a776484d7f5c6a8299c09aed44ecb7a610
rurbano3d <rurbano3d@gmail.com>
2019-12-10 08:28:01 -0300

    Error Handler and Env

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '03d01c5b75d44430039064298ce355a9',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '0350ceb2ea705a',
-    pass: '597193319d0a60',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gym Point <noreply@gympoint.com>',
