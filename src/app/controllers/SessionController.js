9473414d0d83419c375fe1c452b2190af796733f
rurbano3d <rurbano3d@gmail.com>
2019-12-12 18:49:55 -0300

    feat core

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -29,12 +29,13 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, level } = user;
     return res.json({
       user: {
         id,
         name,
         email,
+        level,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
